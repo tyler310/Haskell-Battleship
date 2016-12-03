ff4e0475ff025a5beaed5d7234662602ea3507d8
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2016-12-02 16:18:19 -0800

    updates board if a guess is right

--- battleship.hs
+++ battleship.hs
@@ -5,10 +5,11 @@ module Battleship where
 import Control.Monad
 
 
-p_board = [[(0,1),(6,7)],[(6,8),(0,0)]]             
+p_board = [[(0,1),(6,7)],[(6,8),(0,0)]]
+o_board = []             
 
 check_guess :: (Int, Int) -> [[(Int, Int)]] -> Bool
-check_guess coord lst
+check_guess coord lst 
     | coord `elem` concat lst = True
     | otherwise = False
     
@@ -17,16 +18,18 @@ check_guess coord lst
 -- guess: gets user's guess, send it, update board (hit/miss), signal end of turn
 
 guess = do
-    putStrLn "Please guess"
+    putStrLn "Enter your target (row, column): "
     g <- getLine
     putStrLn ("You guessed " ++ g)
     let input = read g
-    let ans = check_guess input p_board
-    return ans
+    -- need to SEND the guess, remove line below
+    let ans = check_guess input p_board -- replace this with the send guess function
+    return (input, ans)
     
     
-
-
+update_board g hit lst
+    | hit == True = g:lst
+    | otherwise = lst
     
 --p_board :: [(Int, Int)]
 -- generating boats eventually
@@ -34,8 +37,17 @@ guess = do
 
 
 main = forever $ do
-
-    g <- guess
+    -- Guess
+    (g, ans) <- guess
+    -- Update the o_board depending on the result
+    let new_o_board = update_board g ans o_board
+    let o_board = new_o_board
+    
+    -- Accept the guess and check if it's right using check_guess
+    
+    -- Send the True/False (hit/miss) back
+    
+    putStrLn(show o_board)
     guess
 
 
